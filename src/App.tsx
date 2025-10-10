b50bc3c1c8a4fcf959c02cf4394b5d1c171662b2
ajs <140010746+aboodJS@users.noreply.github.com>
2025-10-10 15:22:05 +0300

    [CHANGES] rebuilt the search algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
 import TopBar from "./components/TopBar";
 import SideBar from "./components/SideBar";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const [result, setResult] = useState([]);
+  const [type, setType] = useState("teams");
   const searchRef = useRef(null);
-  async function getData(target) {
-    await fetch(`http://localhost:5000/teams/${target}`)
-      .then((d) => d.json())
-      .then((d) => setResult(d.results));
+  const selectRef = useRef(null);
+  async function getData(target: string) {
+    switch (type) {
+      case "teams":
+        await fetch(
+          `https://www.thesportsdb.com/api/v1/json/123/searchteams.php?t=${target}&s=soccer`
+        )
+          .then((d) => d.json())
+          .then((d) =>
+            setResult(d.teams.filter((t: object) => t.strSport === "Soccer"))
+          );
+
+        console.log(type);
+        console.log(result);
+        console.log(target);
+
+        break;
+      case "players":
+        await fetch(
+          `https://www.thesportsdb.com/api/v1/json/123/searchplayers.php?p=${target}&s=soccer`
+        )
+          .then((d) => d.json())
+          .then((d) => setResult(d.player));
+
+        console.log(type);
+        console.log(result.player);
+        console.log(target);
+        break;
+
+      case "matches":
+        await fetch(
+          `https://www.thesportsdb.com/api/v1/json/123/searchevents.php?e=${target}`
+        )
+          .then((d) => d.json())
+          .then((d) => setResult(d.event));
+
+        console.log(type);
+        console.log(result.event);
+        console.log(target);
+        break;
+    }
   }
+
+  useEffect(() => {
+    setType(selectRef.current.value);
+    console.log(type);
+  }, [type]);
   return (
     <>
       <TopBar></TopBar>
@@ -21,7 +64,7 @@ function App() {
           placeholder="search"
           name=""
           id=""
-        />{" "}
+        />
         <button
           onClick={() => {
             getData(searchRef.current.value);
@@ -30,15 +73,32 @@ function App() {
         >
           search
         </button>
+        <select
+          ref={selectRef}
+          onChange={() => {
+            setType(selectRef.current.value);
+          }}
+          name=""
+          id=""
+        >
+          <option value="teams" defaultValue="true">
+            teams
+          </option>
+          <option value="players">players</option>
+          <option value="matches">matches</option>
+        </select>
       </div>
       <SideBar></SideBar>
 
       <ul className="resultlist">
         {result.map((e: string, i: number) => {
           if (e !== null) {
+            const nameKey = Object.keys(e).find((key) => key.match(/str[A-Z]/));
+            const leagueKey = Object.keys(e).find((key) => key.match(/strL/));
+            console.log(nameKey);
             return (
               <li className="searchresult" key={i}>
-                {e}
+                {e[nameKey]} - {e[leagueKey] || e.strTeam}
               </li>
             );
           }
